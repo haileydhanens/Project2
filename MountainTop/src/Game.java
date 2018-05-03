6558982f0c30d1770c9e942157adb53e0092918a
highdefsquirrelgirl <highdefsquirrelgirl@users.noreply.github.com>
2018-05-02 22:30:53 -0700

    yeet

--- MountainTop/src/Game.java
+++ MountainTop/src/Game.java
@@ -4,19 +4,23 @@
  * and open the template in the editor.
  */
 
+import java.util.Random;
+
 /**
  *
  * @author hdbut_000
  */
 public class Game {
  
-    char[][] board = new char[5][5];        // holds the state of the game
-    int turn;
+    public char[][] board = new char[5][5];        // holds the state of the game
+    public int turn;
+    public boolean gameOver;
     
     public Game(){
         
         board = new char[5][5];
         this.turn= 0;
+        gameOver = false;
         
         for (char[] a : board){             //creates an empty board
             for(char b : a){
@@ -24,8 +28,33 @@ public class Game {
             }
             
         }
+        int number = 0;
+        Game enter= this;
+        char character = 'b';
+        Runnable r = new Player(enter, character, number);
+        (new Thread(new Player( this , 'B' , number))).start();
         
-        (new Thread(new Player())).start();
+    }
+    
+    private int[] randomPos(){
+        Random rand = new Random();
+        
+        int num1 = rand.nextInt(5);
+        int num2 = rand.nextInt(5);
+        
+        int[] array = {num1, num2};
+        
+        return array;
+    }
+    
+    public void turnOver(){
+        
+        if(turn>= 3){
+            turn = 0;
+        }
+        else{
+            turn++;
+        }
         
     }
     
--- MountainTop/src/Player.java
+++ MountainTop/src/Player.java
@@ -12,14 +12,28 @@ public class Player implements Runnable {
 
     Game game;
     char name;
+    boolean hasCarrot;
+    boolean frozen;
+    boolean dead;
+    int canPlay;
     
     public Player(){
         
     }
+    //needs to be changed if making tweety and tazz class
+    public Player(Game game){
+       this.game = game;
+       name= 'B';
+       canPlay=0;
+    }
     
-    public Player(Game game, char name ){
+    public Player(Game game, char name, int canPlay ){
        this.game= game;
        this.name= name;
+       hasCarrot= false;
+       frozen= false;
+       dead= false;
+       this.canPlay = canPlay;
        
        //get starting position
        
@@ -28,7 +42,38 @@ public class Player implements Runnable {
     
     public void run(){
         
-        
-        
+        while(this.game.gameOver == false){
+     
+        takeTurn();    
+            
+            
+            
+            
+            
+            
+        game.turnOver(); // character ends their turn
+        }
     }
+    
+    protected void takeTurn(){
+        
+        if(frozen || dead){             //if the character is frozen spend turn unfreezing
+            
+            frozen = false;
+        }
+        
+        else{
+         
+            boolean foundStep = false;
+            
+            while (!foundStep){
+                //use a rand(7) to pick which direction to go. if direction is valid, foundstep = true and then move there.
+            }
+            
+            
+            
+            
+        }   
+    }
+    
 }
